10c00553cfe37c0d9504490c18141e6b4a2cace8
UrszulaKarsznia <karsznia.urszula.zofia@gmail.com>
2022-01-31 22:00:47 +0100

    Zadanie 1 part one

--- src/com/company/devices/Car.java
+++ src/com/company/devices/Car.java
@@ -1,7 +1,9 @@
 package com.company.devices;
 import com.company.creatures.Human;
 
-public class Car extends Device {
+import java.util.Comparator;
+
+public abstract class Car extends Device {
     public Double engineVolume;
     public String plates;
     public Double value;
@@ -29,13 +31,13 @@ public class Car extends Device {
     public void Sale(Human seller, Human buyer, Double price) {
         if (buyer.cash < price){
             System.out.println("Nie stać Cię");
-        } else if (seller.car != this){
+        } else if (!seller.hasCar(this)){
             System.out.println("Nie ładnie sprzedawać coś, czego się nie ma.. OSZUŚCIE!");
         } else{
             seller.cash += price;
             buyer.cash -= price;
-            seller.car = null;
-            buyer.car = this;
+            seller.removeCar(this);
+            buyer.addCar(this);
             System.out.println("Udało Ci się sprzedać auto za " + price + " PLN");
         }
     }
